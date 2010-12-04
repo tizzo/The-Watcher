f424cba80b37e695fc844978d3a41dd2f33543c2
tizzo <tizzo@Howards-Jawn.local>
2010-12-04 18:39:03 -0500

    adding the watcher

---
+++ fsmonitor.py
@@ -0,0 +1,240 @@
+"""fsmonitor.py Cross-platform file system monitor
+
+How it works:
+- Uses inotify on Linux (kernel 2.6.13 and higher)
+- Uses FileSystemWatcher on Windows (TODO)
+- Uses FSEvents on Mac OS X (10.5 and higher)
+- Falls back to polling
+
+A persistent mode is also supported, in which all metadata is stored in a
+database. This allows you to even track changes when your program wasn't
+running.
+
+Only FSEvents supports looking back in time. For Linux and Windows this means
+that the manual scanning procedure will be used instead until we have caught
+up.
+
+To make this class work consistently, less critical features that are only
+available for specific file system monitors are abstracted away. And other
+features are emulated.
+It comes down to the fact that FSMonitor's API is very simple to use and only
+supports 5 different events: CREATED, MODIFIED, DELETED, MONITORED_DIR_MOVED
+and DROPPED_EVENTS. The last 2 events are only triggered for inotify and
+FSEvents.
+
+This implies that the following features are not available through FSMonitor:
+- inotify:
+  * auto_add: is always assumed to be True (FSEvents has no setting for this)
+  * recursive: is always assumed to be True (FSEvents has no setting for this)
+  * IN_ACCESS, IN_CLOSE_WRITE, IN_CLOSE_NOWRITE, IN_OPEN, IN_DELETE_SELF and
+    IN_IGNORED event aren't supported (FSEvents doesn't support this)
+  * IN_UNMOUNT is also not supported because FSEvents' equivalent
+    (kFSEventStreamEventFlagUnmount) isn't supported in Python
+- FSEvents:
+  * sinceWhen: is always set to kFSEventStreamEventIdSinceNow (inotify has no
+    setting for this)
+  * kFSEventStreamEventFlagMount: is ignored (inotify doesn't support this)
+And the following features are emulated:
+- FSEvents:
+  * inotify's mask, which allows you to listen only to certain events
+"""
+
+
+__author__ = "Wim Leers (work@wimleers.com)"
+__version__ = "$Rev$"
+__date__ = "$Date$"
+__license__ = "GPL"
+
+
+import platform
+import sqlite3
+import threading
+import Queue
+import os
+from pathscanner import PathScanner
+
+
+# Define exceptions.
+class FSMonitorError(Exception): pass
+
+
+class FSMonitor(threading.Thread):
+    """cross-platform file system monitor"""
+
+
+    # Identifiers for each event.
+    EVENTS = {
+        "CREATED"             : 0x00000001,
+        "MODIFIED"            : 0x00000002,
+        "DELETED"             : 0x00000004,
+        "MONITORED_DIR_MOVED" : 0x00000008,
+        "DROPPED_EVENTS"      : 0x00000016,
+    }
+
+
+    def __init__(self, callback, persistent=False, trigger_events_for_initial_scan=False, ignored_dirs=[], dbfile="fsmonitor.db"):
+        self.persistent                      = persistent
+        self.trigger_events_for_initial_scan = trigger_events_for_initial_scan
+        self.monitored_paths                 = {}
+        self.dbfile                          = dbfile
+        self.dbcon                           = None
+        self.dbcur                           = None
+        self.pathscanner                     = None
+        self.ignored_dirs                    = ignored_dirs
+        self.callback                        = callback
+        self.lock                            = threading.Lock()
+        self.add_queue                       = Queue.Queue()
+        self.remove_queue                    = Queue.Queue()
+        self.die                             = False
+        threading.Thread.__init__(self)
+
+
+    def run(self):
+        """start the file system monitor (starts a separate thread)"""
+        raise NotImplemented
+
+
+    def add_dir(self, path, event_mask):
+        """add a directory to monitor"""
+        self.lock.acquire()
+        self.add_queue.put((path, event_mask))
+        self.lock.release()
+
+
+    def __add_dir(self, path, event_mask):
+        raise NotImplemented
+
+
+    def remove_dir(self, path):
+        """stop monitoring a directory"""
+        self.lock.acquire()
+        self.remove_queue.put(path)
+        self.lock.release()
+
+
+    def __remove_dir(self, path):
+        raise NotImplemented
+
+
+    def generate_missed_events(self, path, event_mask=None):
+        """generate the missed events for a persistent DB"""
+        for event_path, result in self.pathscanner.scan_tree(path):
+            self.trigger_events_for_pathscanner_result(path, event_path, result, event_mask)
+
+
+    def stop(self):
+        """stop the file system monitor (stops the separate thread)"""
+        raise NotImplemented
+
+
+    def purge_dir(self, path):
+        """purge the metadata for a monitored directory
+        
+        Only possible if this is a persistent DB.
+        """
+        if self.persistent:
+            self.pathscanner.purge_path(path)
+
+
+    def trigger_event(self, monitored_path, event_path, event):
+        """trigger one of the standardized events"""
+        if callable(self.callback):
+            self.callback(monitored_path, event_path, event)
+
+
+    def setup(self):
+        """set up the database and pathscanner"""
+        # Database.
+        if self.dbcur is None:
+            self.dbcon = sqlite3.connect(self.dbfile)
+            self.dbcur = self.dbcon.cursor()
+        # PathScanner.
+        if self.persistent == True and self.dbcur is not None:
+            self.pathscanner = PathScanner(self.dbcon, self.ignored_dirs, "pathscanner")
+
+
+    def trigger_events_for_pathscanner_result(self, monitored_path, event_path, result, event_mask=None):
+        """trigger events for pathscanner result"""
+        if event_mask is None:
+            event_mask = self.monitored_paths[monitored_path].event_mask
+        if event_mask & FSMonitor.CREATED:
+            for filename in result["created"]:
+                self.trigger_event(monitored_path, os.path.join(event_path, filename), self.CREATED)
+        if event_mask & FSMonitor.MODIFIED:
+            for filename in result["modified"]:
+                self.trigger_event(monitored_path, os.path.join(event_path, filename), self.MODIFIED)                
+        if event_mask & FSMonitor.DELETED:
+            for filename in result["deleted"]:
+                self.trigger_event(monitored_path, os.path.join(event_path, filename), self.DELETED)
+
+
+    def is_in_ignored_directory(self, path):
+        """checks if the given path is in an ignored directory"""
+        dirs = os.path.split(path)
+        for dir in dirs:
+            if dir in self.ignored_dirs:
+                return True
+        return False
+
+
+class MonitoredPath(object):
+    """A simple container for all metadata related to a monitored path"""
+    def __init__(self, path, event_mask, fsmonitor_ref=None):
+        self.path = path
+        self.event_mask = event_mask
+        self.fsmonitor_ref = fsmonitor_ref
+        self.monitoring = False
+
+
+def __get_class_reference(modulename, classname):
+    """get a reference to a class"""
+    module = __import__(modulename, globals(), locals(), [classname])
+    class_reference = getattr(module, classname)
+    return class_reference
+
+
+def get_fsmonitor():
+    """get the FSMonitor for the current platform"""
+    system = platform.system()
+    if system == "Linux":
+        kernel = platform.release().split(".")
+        # Available in Linux kernel 2.6.13 and higher.
+        if int(kernel[0]) == 2 and int(kernel[1]) == 6 and kernel[2][:2] >= 13:
+            return __get_class_reference("fsmonitor_inotify", "FSMonitorInotify")
+    elif system == "Windows":
+        # See:
+        # - http://timgolden.me.uk/python/win32_how_do_i/watch_directory_for_changes.html
+        # - http://code.activestate.com/recipes/156178/
+        # - http://stackoverflow.com/questions/339776/asynchronous-readdirectorychangesw
+        pass
+    elif system == "Darwin":
+        (release, version_info, machine) = platform.mac_ver()
+        major = release.split(".")[1]
+        # Available in Mac OS X 10.5 and higher.
+        if (major >= 5):
+            return __get_class_reference("fsmonitor_fsevents", "FSMonitorFSEvents")
+
+    # Default to a polling mechanism
+    return __get_class_reference("fsmonitor_polling", "FSMonitorPolling")
+
+
+# Make EVENTS' members directly accessible through the class dictionary.
+for name, mask in FSMonitor.EVENTS.iteritems():
+    setattr(FSMonitor, name, mask)
+
+
+
+
+if __name__ == "__main__":
+    import time
+
+    def callbackfunc(monitored_path, event_path, event):
+        print "CALLBACK FIRED, params: monitored_path=%s', event_path='%s', event='%d'" % (monitored_path, event_path, event)
+
+    fsmonitor_class = get_fsmonitor()
+    print "Using class", fsmonitor_class
+    fsmonitor = fsmonitor_class(callbackfunc, True)
+    fsmonitor.start()
+    fsmonitor.add_dir("/Users/wimleers/Downloads", FSMonitor.CREATED | FSMonitor.MODIFIED | FSMonitor.DELETED)
+    time.sleep(30)
+    fsmonitor.stop()
---
+++ fsmonitor_fsevents.py
@@ -0,0 +1,226 @@
+"""fsmonitor_fsevents.py FSMonitor subclass for FSEvents on Mac OS X >= 10.5
+
+Always works in persistent mode by (FSEvent's) design.
+"""
+
+
+__author__ = "Wim Leers (work@wimleers.com)"
+__version__ = "$Rev$"
+__date__ = "$Date$"
+__license__ = "GPL"
+
+
+from fsmonitor import *
+from FSEvents import kCFAllocatorDefault, \
+                     CFRunLoopGetCurrent, \
+                     kCFRunLoopDefaultMode, \
+                     CFRunLoopRun, \
+                     CFRunLoopStop, \
+                     CFRunLoopAddTimer, \
+                     CFRunLoopTimerCreate, \
+                     CFAbsoluteTimeGetCurrent, \
+                     NSAutoreleasePool, \
+                     kFSEventStreamEventIdSinceNow, \
+                     kFSEventStreamCreateFlagWatchRoot, \
+                     kFSEventStreamEventFlagMustScanSubDirs, \
+                     kFSEventStreamEventFlagUserDropped, \
+                     kFSEventStreamEventFlagKernelDropped, \
+                     kFSEventStreamEventFlagRootChanged, \
+                     FSEventStreamScheduleWithRunLoop, \
+                     FSEventStreamCreate, \
+                     FSEventStreamStart, \
+                     FSEventStreamStop, \
+                     FSEventStreamInvalidate, \
+                     FSEventStreamRelease, \
+                     FSEventStreamShow
+
+
+# Define exceptions.
+class FSMonitorFSEventsError(FSMonitorError): pass
+class MonitorError(FSMonitorFSEventsError): pass
+class CouldNotStartError(FSMonitorFSEventsError): pass
+
+
+class FSMonitorFSEvents(FSMonitor):
+    """FSEvents support for FSMonitor"""
+
+
+    # These 3 settings are hardcoded. See FSMonitor's documentation for an
+    # explanation.
+    latency = 1.0
+    sinceWhen = kFSEventStreamEventIdSinceNow
+    flags = kFSEventStreamCreateFlagWatchRoot
+
+
+    def __init__(self, callback, persistent=True, trigger_events_for_initial_scan=False, ignored_dirs=[], dbfile="fsmonitor.db"):
+        FSMonitor.__init__(self, callback, True, trigger_events_for_initial_scan, ignored_dirs, dbfile)
+        self.latest_event_id = None
+        self.auto_release_pool = None
+
+
+    def __add_dir(self, path, event_mask):
+        """override of FSMonitor.__add_dir()"""
+        if self.trigger_events_for_initial_scan and self.persistent:
+            FSMonitor.generate_missed_events(self, path, event_mask)
+        else:
+            # Perform an initial scan of the directory structure. If this has
+            # already been done, then it will return immediately.
+            self.pathscanner.initial_scan(path)
+
+        # Use the FSEvents API to monitor a directory.
+        streamRef = FSEventStreamCreate(kCFAllocatorDefault,
+                                        self.__fsevents_callback,
+                                        path,
+                                        [path],
+                                        self.__class__.sinceWhen,
+                                        self.__class__.latency,
+                                        self.__class__.flags)
+        # Debug output.
+        #FSEventStreamShow(streamRef)
+
+        if streamRef is None:
+            raise MonitorError, "Could not monitor %s" % path
+            return None
+        else:
+            self.monitored_paths[path] = MonitoredPath(path, event_mask, streamRef)
+            return self.monitored_paths[path]
+
+
+    def __remove_dir(self, path):
+        """override of FSMonitor.__remove_dir()"""
+        if path in self.monitored_paths.keys():
+            streamRef = self.monitored_paths[path].fsmonitor_ref
+            # Stop, unschedule, invalidate and release the stream refs.
+            FSEventStreamStop(streamRef)
+            # We don't use FSEventStreamUnscheduleFromRunLoop prior to
+            # invalidating the stream, because invalidating the stream
+            # automatically unschedules the stream from all run loops.
+            FSEventStreamInvalidate(streamRef)
+            FSEventStreamRelease(streamRef)
+
+            del self.monitored_paths[path]
+
+
+    def run(self):
+        # Necessary because we're using PyObjC in a thread other than the main
+        # thread.
+        self.auto_release_pool = NSAutoreleasePool.alloc().init()
+
+        # Setup. Ensure that this isn't interleaved with any other thread, so
+        # that the DB setup continues as expected.
+        self.lock.acquire()
+        FSMonitor.setup(self)
+        self.lock.release()
+
+        # Set up a callback to a function that process the queues frequently.
+        CFRunLoopAddTimer(
+           CFRunLoopGetCurrent(),
+           CFRunLoopTimerCreate(None, CFAbsoluteTimeGetCurrent(), 0.5, 0, 0, self.__process_queues, None),
+           kCFRunLoopDefaultMode
+        )
+
+        # Start the run loop.
+        CFRunLoopRun()
+
+
+    def stop(self):
+        """override of FSMonitor.stop()"""
+
+        # Let the thread know it should die.
+        self.lock.acquire()
+        self.die = True
+        self.lock.release()
+
+        # Stop monitoring each monitored path.
+        for path in self.monitored_paths.keys():
+            self.__remove_dir(path)
+
+        # Store the latest event ID so we know where we left off.
+        # TODO: separate table in DB to store this?
+
+        # Delete the auto release pool.
+        del self.auto_release_pool
+
+
+    def __process_queues(self, timer, context):
+        # Die when asked to.
+        self.lock.acquire()
+        if self.die:
+            CFRunLoopStop(CFRunLoopGetCurrent())
+        self.lock.release()
+
+
+        # Process add queue.
+        self.lock.acquire()
+        if not self.add_queue.empty():
+            (path, event_mask) = self.add_queue.get()
+            self.lock.release()
+            self.__add_dir(path, event_mask)
+        else:
+            self.lock.release()
+
+        # Process remove queue.
+        self.lock.acquire()
+        if not self.remove_queue.empty():
+            path = self.add_queue.get()
+            self.lock.release()
+            self.__remove_dir(path)
+        else:
+            self.lock.release()
+
+        # Ensure all monitored paths are actually being monitored. If they're
+        # not yet being monitored, start doing so.
+        for path in self.monitored_paths.keys():
+            if self.monitored_paths[path].monitoring:
+                continue
+            streamRef = self.monitored_paths[path].fsmonitor_ref
+            
+            # Schedule stream on a loop.
+            FSEventStreamScheduleWithRunLoop(streamRef, CFRunLoopGetCurrent(), kCFRunLoopDefaultMode)
+
+            # Register with the FS Events service to receive events.
+            started = FSEventStreamStart(streamRef)
+            if not started:
+                raise CouldNotStartError
+            else:
+                self.monitored_paths[path].monitoring = True
+
+            # Generate the missed events.
+            # TODO: use FSEvents' sinceWhen parameter instead of the current
+            # inefficient scanning method.
+            if self.persistent:
+                FSMonitor.generate_missed_events(self, path)
+
+
+    def __fsevents_callback(self, streamRef, clientCallBackInfo, numEvents, eventPaths, eventFlags, eventIDs):
+        """private callback function for use with FSEventStreamCreate"""
+        # Details of the used flags can be found in FSEvents.h.
+        monitored_path = clientCallBackInfo
+
+        for i in range(numEvents):
+            event_path = eventPaths[i]
+            self.latest_event_id = eventIDs[i]
+
+            # Strip trailing slash
+            if event_path[-1] == '/':
+                event_path = event_path[:-1]
+
+            if FSMonitor.is_in_ignored_directory(self, event_path):
+                return
+
+            # Trigger the appropriate events.
+            if eventFlags[i] & kFSEventStreamEventFlagUserDropped:
+                FSMonitor.trigger_event(self, monitored_path, None, FSMonitor.DROPPED_EVENTS)
+
+            elif eventFlags[i] & kFSEventStreamEventFlagKernelDropped:
+                FSMonitor.trigger_event(self, monitored_path, None, FSMonitor.DROPPED_EVENTS)
+
+            elif eventFlags[i] & kFSEventStreamEventFlagRootChanged:
+                FSMonitor.trigger_event(self, monitored_path, event_path, FSMonitor.MONITORED_DIR_MOVED)
+
+            elif eventFlags[i] & kFSEventStreamEventFlagMustScanSubDirs:
+                result = self.pathscanner.scan_tree(event_path)
+                FSMonitor.trigger_events_for_pathscanner_result(self, monitored_path, event_path, result)
+            else:
+                result = self.pathscanner.scan(event_path)
+                FSMonitor.trigger_events_for_pathscanner_result(self, monitored_path, event_path, result)
